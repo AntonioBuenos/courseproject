2b6586f1819b0183097e79726685fb97b14d0e1f
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-08-29 19:57:43 +0300

    RowMapper for Users added.

--- src/main/java/by/smirnov/courseproject/dao/UserRowMapper.java
+++ src/main/java/by/smirnov/courseproject/dao/UserRowMapper.java
@@ -6,9 +6,24 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static by.smirnov.courseproject.dao.user.UserTableColumns.*;
+
 public class UserRowMapper implements RowMapper<User> {
+
+    //Logger to be added
     @Override
-    public User mapRow(ResultSet resultSet, int i) throws SQLException {
-        return null;
+    public User mapRow(ResultSet rs, int i) throws SQLException {
+        User user = new User();
+
+        user.setId(rs.getLong(ID));
+        user.setFirstName(rs.getString(NAME));
+        user.setLastName(rs.getString(SURNAME));
+        user.setUserRole(rs.getString(ROLE));
+        user.setCreationDate(rs.getTimestamp(CREATED));
+        user.setModificationDate(rs.getTimestamp(CHANGED));
+        user.setDeleted(rs.getBoolean(IS_DELETED));
+        user.setTerminationDate(rs.getTimestamp(TERMINATED));
+
+        return user;
     }
 }
